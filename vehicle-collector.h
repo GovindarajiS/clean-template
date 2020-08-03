b10ab5948a6b58d23f9c3e1038ff0328b362e8bb
GovindarajiS <68851109+GovindarajiS@users.noreply.github.com>
2020-08-03 17:26:49 +0530

    Create vehicle-collector.h

---
+++ vehicle-collector.h
@@ -0,0 +1,34 @@
+enum measure {
+ float motor_temp;
+ float battery_pc;
+ float battery_temp;
+};
+
+struct telematics {
+  int vehicle_id;
+  int type;
+  float measurement;
+};
+
+struct inventory {
+  int vehicle_id;
+  float motor_temp;
+  float battery_pc;
+  float battery_temp;
+};
+
+telematics_value_passing_to_inventory(struct telematics t) {
+
+struct inventory i;
+
+i.vehicle_id = t.vehicle_id;
+if (t.type == motor_temp) {
+    i.motor_temp = t.measurement;
+    }
+    else if (t.type == battery_temp) {
+    i.battery_temp = t.measurement;
+    }
+    else {
+      i.battery_pc = t.battery_pc;
+    }
+ }
