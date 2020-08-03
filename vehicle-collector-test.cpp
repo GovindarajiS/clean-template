04a6b8b4b769ae6ed99c5b4f9c941afdeaafd3fa
GovindarajiS <68851109+GovindarajiS@users.noreply.github.com>
2020-08-03 17:02:42 +0530

    Update vehicle-collector-test.cpp

--- vehicle-collector-test.cpp
+++ vehicle-collector-test.cpp
@@ -1,11 +1,18 @@
 #include <gtest/gtest.h>
- 
+#include "vehicle-collector.h"
+
 TEST(COLLECT, when_a_vehicle_report_a_measurement_it_is_fetched_stored_with_vehicle_id) {
     struct vehicle {
           int id;
           float temp;
           }v1;
     ASSERT_NEAR(v1.temp, get_vehicle_measurement(vehicle_id, motor_temp, 0.001));
+}
+
+TEST(COLLECT, Copy_telematics_to_invertory) {
+ struct telematics t = {30, motor_temp, 130};
+ telematics_value_passing_to_inventory(telematics t);
+ ASSER_NEAR(130, i.motor_temp, 0.001);             
 }
 
 int main(int argc, char **argv) {
